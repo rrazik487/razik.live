2e29b9bbc3d41869494937d42da0be0b6160d403
Razik <rrazik487@gmail.com>
2025-04-10 15:02:53 +0000

    Fix GitHub Pages deployment: upload build artifact1

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // If you want to set a custom base path, e.g., for GitHub Pages repo name:
-    // basePath: '/your-repo-name',
-    // assetPrefix: '/your-repo-name/',
   };
   
   module.exports = nextConfig;
+  
