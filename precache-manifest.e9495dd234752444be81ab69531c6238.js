8fb98bc87def5fca74238fc82dd9476e376371b0
Razik <rrazik487@gmail.com>
2025-04-12 15:16:09 +0000

    Updates

---
+++ precache-manifest.e9495dd234752444be81ab69531c6238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0c56e741b34b1d984b8242510cc959",
+    "url": "/razik.live/index.html"
+  },
+  {
+    "revision": "012217065ad7a40b7d2c",
+    "url": "/razik.live/static/css/main.60ec272c.chunk.css"
+  },
+  {
+    "revision": "b1c0880bf4b1ff2ffb98",
+    "url": "/razik.live/static/js/2.b83e299d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/razik.live/static/js/2.b83e299d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "012217065ad7a40b7d2c",
+    "url": "/razik.live/static/js/main.016185a1.chunk.js"
+  },
+  {
+    "revision": "5bed90d9d7e3a74d3a21",
+    "url": "/razik.live/static/js/runtime-main.59cc84e8.js"
+  }
+]);
